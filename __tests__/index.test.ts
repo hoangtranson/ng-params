de1d91d383eba7d9c9f7e0cf3704d5414dd2c754
Tran Son Hoang <son.hoang01@gmail.com>
2021-03-22 22:42:49 +0800

    feat: 🎸 setup params class + get queries

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,24 @@
-import { Greeter } from '../src';
+import PARAMS from '../src';
 
-test('My Greeter', () => {
-  expect(Greeter('Subin')).toBe('Hello Subin');
-});
+describe('Ng Params Pass Cases', () => {
+  let windowSpy: any;
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, "window", "get");
+  });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
+  });
+
+
+  test('first case', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        search: "?so_medium=StackOverflow&so_source=SiteNav"
+      }
+    }));
+    const _url = new PARAMS();
+    expect(_url.queries.so_medium).toBe('StackOverflow');
+  });
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { PARAM_OPTIONS } from "./types/params.t";
+import splitOnFirst from "./utils/splitOnFirst.helper";
+export default class PARAMS {
+  
+    private _l: string;
+
+    constructor(_option: PARAM_OPTIONS | {} = {}) {
+
+        if (typeof window === 'undefined') {
+            throw new Error("window is not defined yet!!!");
+        }
+
+        if (typeof window.location.search !== 'string' || !window.location.search) {
+            throw new Error("location search is not string type!!!");
+        }
+
+        this._l = window.location.search.trim().replace(/^[?#&]/, '');
+    }
+
+    get location() {
+        return this._l;
+    }
+
+    get queries() {
+        return this._l.split('&').filter( item => item).reduce( (acc, current) => {
+            let [key, value] = splitOnFirst(current, '=');
+            acc[key] = value;
+            return acc;
+        }, Object.create(null));
+    }
+
+    public pick(list: string[]) {
+
+    }
+
+    public exclude(list: string[]) {
+
+    }
+
+    public remake(object: Object) {
+        
+    }
+}
